4fa0db3a21e0ace3d30c1b42dce69295b82e4411
OanaIgnat <oignat@umich.edu>
2022-04-24 18:22:00 -0400

    Add ground truth probability to CLIP evaluation

--- evaluate_clip_embeddings.py
+++ evaluate_clip_embeddings.py
@@ -18,19 +18,24 @@ def main() -> None:
 
         similarity = video_features @ text_features.T
 
-        probs = (similarity * TEMPERATURE).softmax(dim=-1)
+        target = torch.arange(len(similarity), device="cuda")
 
-        target = torch.arange(len(probs), device="cuda")
-
-        sorted_predicted_positions = probs.argsort(dim=-1, descending=True)
+        sorted_predicted_positions = similarity.argsort(dim=-1, descending=True)
         ranks = (sorted_predicted_positions == target.unsqueeze(dim=-1)).nonzero(as_tuple=True)[1]  # noqa
 
         print("Mean rank:", ranks.to(torch.float).mean())
         print("Median rank:", ranks.median())
 
-        print("R@1:", (probs.argmax(dim=-1) == target).to(torch.float).mean())
-        print("R@5:", (probs.topk(5)[1] == target.unsqueeze(dim=-1)).sum(dim=-1).to(torch.float).mean())  # noqa
-        print("R@10:", (probs.topk(10)[1] == target.unsqueeze(dim=-1)).sum(dim=-1).to(torch.float).mean())  # noqa
+        print("R@1:", (similarity.argmax(dim=-1) == target).to(torch.float).mean())
+        print("R@5:", (similarity.topk(5)[1] == target.unsqueeze(dim=-1)).sum(dim=-1).to(torch.float).mean())  # noqa
+        print("R@10:", (similarity.topk(10)[1] == target.unsqueeze(dim=-1)).sum(dim=-1).to(torch.float).mean())  # noqa
+
+        probs = (similarity * TEMPERATURE).softmax(dim=-1)
+
+        ground_truth_probs = probs.diag()
+
+        print("Mean ground truth probability:", ground_truth_probs.mean())
+        print("Median ground truth probability:", ground_truth_probs.median())
 
 
 if __name__ == '__main__':
